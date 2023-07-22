46928fb1b897030091db45fa07c0823844fec27b
Dakota Schramm <william.schramm11@gmail.com>
2023-07-22 18:01:46 -0500

    Create smoke test page.

---
+++ components/HelloWorld.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-blue-300 text-white text-xl">
+    Hello world!
+  </div>
+</template>
---
+++ pages/hello_world.vue
@@ -0,0 +1,10 @@
+<script>
+  import HelloWorld from '@@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    Test
+  </div>
+  <HelloWorld />
+</template>
