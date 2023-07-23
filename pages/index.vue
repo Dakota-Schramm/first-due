3d0579672a33409b0e1c60c65d57c4658e49e404
Dakota Schramm <william.schramm11@gmail.com>
2023-07-23 17:49:43 -0500

    Add smoke test for index page.

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="mb-6">
+    <h1 class="font-bold text-center underline">My First Vue Clock</h1>
+  </header>
+  <main class="flex flex-col items-center justify-center w-full h-screen text-lg text-white bg-blue-600">
+    <div class="text-3xl font-bold">12:00</div>
+    <p>This is my custom message!</p>
+  </main>
+  <footer class="mt-6">
+    <p class="text-base font-light text-center ">Created by Dakota Schramm</p>
+  </footer>
+</template>
