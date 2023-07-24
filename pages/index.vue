be2f2d7d07e1d36c6d417d0580eef3b08c297b3e
Dakota Schramm <william.schramm11@gmail.com>
2023-07-23 21:23:49 -0500

    Add counting down timer.

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,32 @@
 <script setup>
-import { ref } from 'vue'
+import { reactive, watch, onMounted } from 'vue'
 import convertDisplayTime from '~/utils/convertDisplayTime'
 
-const currentTime = new Date()
-const convertedTime = convertDisplayTime(currentTime)
-const time = ref(convertedTime)
+const ONE_SECOND = 1000
+
+const addSecondsToTime = (date, seconds) => {
+  return new Date(date.getTime() + seconds)
+}
+
+const now = new Date()
+const desiredTime = addSecondsToTime(now, 15)
+
+const countDownTime = desiredTime.getTime() - now.getTime()
+
+const state = reactive({
+  time: countDownTime,
+  message: 'This is my custom message!'
+})
+
+watch(state, () => {
+  if (state.time !== 0) incrementTime()
+}, { immediate: true })
+
+function incrementTime() {
+  if (state.time > 0) setTimeout(() => { state.time-- }, ONE_SECOND);
+  else clearInterval(interval)
+}
+
 </script>
 
 <template>
@@ -12,8 +34,12 @@ const time = ref(convertedTime)
     <h1 class="font-bold text-center underline">My First Vue Clock</h1>
   </header>
   <main class="flex flex-col items-center justify-center w-full h-screen text-lg text-white bg-blue-600">
-    <div class="text-3xl font-bold">{{ time }}</div>
-    <p>This is my custom message!</p>
+    <div class="text-3xl font-bold">
+      {{ convertDisplayTime(state.time) }}
+    </div>
+    <p>
+      {{ state.message }}
+    </p>
   </main>
   <footer class="mt-6">
     <p class="text-base font-light text-center ">Created by Dakota Schramm</p>
--- utils/convertDisplayTime.ts
+++ utils/convertDisplayTime.ts
@@ -1,10 +1,17 @@
+const SECONDS_IN_MINUTE = 60;
+const SECONDS_IN_HOUR = SECONDS_IN_MINUTE * 60;
+
 // We want to convert the currentTime such that it only displays
 // the hour, minute, and second. We also want this to be in
 // standardTime, not military time
-export default function convertDisplayTime(datetime: Date) {
-  const hour = getHour(datetime.getHours());
-  const minute = getMinute(datetime.getMinutes());
-  const second = getSecond(datetime.getSeconds());
+export default function convertDisplayTime(datetime: number) {
+  const calcHours = Math.floor(datetime / SECONDS_IN_HOUR);
+  const calcMinutes = Math.floor(datetime / SECONDS_IN_MINUTE);
+  const calcSeconds = datetime % SECONDS_IN_MINUTE;
+
+  const hour = getHour(calcHours);
+  const minute = getMinute(calcMinutes);
+  const second = getSecond(calcSeconds);
 
   return `${hour}:${minute}:${second}`
 }
@@ -15,11 +22,11 @@ function getHour(hour: number) {
 }
 
 function getMinute(minute: number) {
-  if (minute > 10) return minute
+  if (minute >= 10) return minute
   else return `0${minute}`;
 }
 
 function getSecond(second: number) {
-  if (second > 10) return second 
+  if (second >= 10) return second 
   else return `0${second}`;
 }
