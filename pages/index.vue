7bba1140f2145f90ef23ceda4fc560f830a31c78
Dakota Schramm <william.schramm11@gmail.com>
2023-07-24 08:51:13 -0500

    Add conditional rendering based on timer.

--- pages/index.vue
+++ pages/index.vue
@@ -14,12 +14,12 @@ const addSecondsToTime = (date, seconds) => {
 // const countDownTime = desiredTime.getTime() - now.getTime()
 
 const state = reactive([
-  { time: 15, message: 'NUMBAH ONE' },
-  { time: 15, message: 'Number two :^)' }
+  { time: 3, message: 'I love Abby Hickman!' },
+  { time: 3, message: 'She is my snugbug. :^)' }
 ])
 
 watch(state, () => {
-  if (state.length < 0) return;
+  if (state.length <= 0) return;
 
   const time = state[0].time;
   if (0 <= time) incrementTime()
@@ -39,13 +39,16 @@ function incrementTime() {
   <header class="mb-6">
     <h1 class="font-bold text-center underline">My First Vue Clock</h1>
   </header>
-  <main class="flex flex-col items-center justify-center w-full h-screen text-lg text-white bg-blue-600">
+  <main v-if="0 < state.length" class="flex flex-col items-center justify-center w-full h-screen text-lg text-white bg-blue-600">
     <div class="text-3xl font-bold">
       {{ convertDisplayTime(state[0].time) }}
     </div>
     <p>
       {{ state[0].message }}
     </p>
+  </main>
+  <main v-else>
+    No messages to display!
   </main>
   <footer class="mt-6">
     <p class="text-base font-light text-center ">Created by Dakota Schramm</p>
